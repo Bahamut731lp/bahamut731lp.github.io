69990567315b89adee30eb0cec3b0a2a0c9fc7ca
Bahamut731lp <heindrich73@gmail.com>
2021-05-26 01:40:53 +0200

    Přidání J+O

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
     darkMode: "media",
     purge: ["./docs/**/*.html"],
     theme: {
-    extend: {},
+        extend: {
+            colors: {
+                "jpluso": "#ed5728",
+                "jpluso-300": "#de4617"
+            }
+        },
     },
     variants: {},
     plugins: [],
